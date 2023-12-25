eec096f031860aae533c5536f63a1684efd0490b
Shio703 <shiodjamurashvili26@gmail.com>
2023-12-25 23:32:29 +0400

    Categories and some posts added also added sidebar

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "@/components/pagination/Pagination"
+import Image from "next/image";
+import Card from "../card/Card";
 
 const Header = () => {
-  return <div className={styles.container}><Pagination/></div>;
+  return <div className={styles.container}>
+    <h1 className={styles.title}>Recent Posts</h1>
+    <div className={styles.posts}>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+    <Pagination/></div>;
 };
 
 export default Header;
