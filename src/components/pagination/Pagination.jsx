140f6b7d4b7bf4eee91fb4bf823308cb09e51114
Shio703 <shiodjamurashvili26@gmail.com>
2023-12-26 16:24:19 +0400

    MainPage Primary template completed

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import styles from "./pagination.module.css";
 
 const Header = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return <div className={styles.container}>
+    <button className={styles.button}>Previous</button>
+    <button className={styles.button}>Next</button>
+  </div>;
 };
 
 export default Header;
