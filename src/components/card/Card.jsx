0c514535a6b0b7a4091c8a82b833c666193bf72b
Shio703 <shiodjamurashvili26@gmail.com>
2024-04-30 20:48:09 +0400

    in pagination system post fetching and map function error fixed finally:))

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,9 +3,9 @@ import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
         <Image src="/p1.jpeg" alt="" fill className={styles.image} />
       </div>
@@ -14,14 +14,18 @@ const Card = () => {
           <span className={styles.date}>11.02.2023</span>
           <span className={styles.category}> Culture</span>
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <Link href={"/"}>
+          <h1>{item.title}</h1>
+        </Link>
         <p className={styles.desc}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos atque at
           sapiente ducimus doloribus, adipisci fugit aliquid repudiandae debitis
           minima quibusdam sunt! Rem maiores est cupiditate minima, impedit
           veritatis voluptatum.
         </p>
-        <Link href="/" className={styles.link}>Read More</Link>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,11 +1,29 @@
+"use client";
 import React from "react";
 import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
 
-const Header = () => {
-  return <div className={styles.container}>
-    <button className={styles.button}>Previous</button>
-    <button className={styles.button}>Next</button>
-  </div>;
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <button
+        disabled={!hasPrev}
+        className={styles.button}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        disabled={!hasNext}
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
-export default Header;
+export default Pagination;
